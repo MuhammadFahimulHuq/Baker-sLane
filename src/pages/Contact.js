5d69d0d8c75b12d13351a15b58611ea1e1e731c3
unknown <fahimul.huq85@gmail.com>
2022-02-24 00:36:55 +0600

    contact updating...

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ContactNav } from '../components/ContactNav'
 import { Navbar } from '../components/Navbar'
 import '../pages/Contact.css'
 export const Contact = () => {
@@ -6,9 +7,31 @@ export const Contact = () => {
     <div className='contact'>
 
          <div className='frame-container'>
-        <div></div>
- <Navbar/>
+        <div>
+
+        </div>
+        <Navbar/>
+      </div >
+     
+      <h3>Contact Us
+      <div className='line'> </div>
+      </h3>
+      <div className='contact-nav-container'>
+      <ContactNav />
+        <h3>Connect with us on social media</h3>
+        <div className='line'> </div>
       </div>
-        <h3>Contact</h3></div>
+    <div className='form-bg'>
+<input placeholder='First Name'/>
+<input placeholder='Last Name'/>
+<input placeholder='Email'/>
+<input placeholder='Phone No'/>
+<input type="text" placeholder='Message'/>
+<button>send</button>
+    </div>
+     
+        
+        
+        </div>
   )
 }
